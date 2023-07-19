f272d7ce1c029b7d228c5609b0aec33501386ee3
saidazimov720 <saidazimovnurbek01@gmail.com>
2023-07-19 22:23:30 +0500

    fundamental changes

--- script.js
+++ script.js
@@ -32,3 +32,25 @@ function ism(username) {
   usernames.push(username);
   return true; 
 }
+
+// let usernames = [];
+
+//       function checkAvailability() {
+//         const username = document.getElementById('usernameInput').value;
+//         const isAvailable = isUsernameAvailable(username);
+
+//         const messageElement = document.getElementById('availabilityMessage');
+//         messageElement.textContent = isAvailable
+//           ? 'This username is available'
+//           : 'This username has already been taken';
+//       }
+
+//       function isUsernameAvailable(username) {
+//         for (let name of usernames) {
+//           if (name === username) {
+//             return false; // Username already exists
+//           }
+//         }
+//         usernames.push(username); // Save the username
+//         return true; // Username is available
+//       }
