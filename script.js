fddd7c69d17b19c79cd033cd68ed2c1fb0378465
saidazimov720 <saidazimovnurbek01@gmail.com>
2023-07-19 15:23:07 +0500

    add the if else statement

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ function validate() {
     var messagel = document.getElementById("message");
 
     var names = ["Nurbek", "Sunnat", "Javohir", "Rustam"];
-    
+    if (names.includes(username)) {
+        messagel.innerHTML = "Username has already been taken";
+    }
+    else{
+        messagel.innerHTML = "Username is available";
+    }
 }
