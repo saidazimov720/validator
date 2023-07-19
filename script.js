670ed164e3079e5ee7bd4623fbb3466349094157
saidazimov720 <saidazimovnurbek01@gmail.com>
2023-07-19 22:30:44 +0500

    some changes

--- script.js
+++ script.js
@@ -1,56 +1,20 @@
-// function validate() {
-//     var username = document.getElementById("username").value;
-//     var message = document.getElementById("message");
+let usernames = [];
 
-//     var names = ["Nurbek", "Sunnat", "Javohir", "Rustam"];
-//     if (names.includes(username)) {
-//         message.innerHTML = "Username has already been taken.";
-//     }
-//     else{
-//         message.innerHTML = "Username is available.";
-//     }
-// }
+function checkAvailability() {
+  const username = document.getElementById('usernameInput').value;
+  const isAvailable = isUsernameAvailable(username);
 
-let names = [];
-
-function tekshir() {
-  const username = document.getElementById('username').value;
-  const isAvailable = ism(username);
-
-  const message = document.getElementById('natija');
-  message.textContent = isAvailable
-    ? 'This username is available.'
-    : 'This username has already been taken.';
+  const messageElement = document.getElementById('availabilityMessage');
+  messageElement.textContent = isAvailable
+    ? 'This username is available'
+    : 'This username has already been taken';
 }
 
-function ism(username) {
-  for (let name of names) {
+function isUsernameAvailable(username) {
+  for (let name of usernames) {
     if (name === username) {
-      return false;
-        }
+      return false; // Username already exists
+    }
   }
-  usernames.push(username);
-  return true; 
-}
-
-// let usernames = [];
-
-//       function checkAvailability() {
-//         const username = document.getElementById('usernameInput').value;
-//         const isAvailable = isUsernameAvailable(username);
-
-//         const messageElement = document.getElementById('availabilityMessage');
-//         messageElement.textContent = isAvailable
-//           ? 'This username is available'
-//           : 'This username has already been taken';
-//       }
-
-//       function isUsernameAvailable(username) {
-//         for (let name of usernames) {
-//           if (name === username) {
-//             return false; // Username already exists
-//           }
-//         }
-//         usernames.push(username); // Save the username
-//         return true; // Username is available
-//       }
+  usernames.push(username); // Save the username
+  return true; // Username is available
