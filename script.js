21b6cc7202d98c9835d469228e14a8999b82508f
saidazimov720 <saidazimovnurbek01@gmail.com>
2023-07-19 20:02:51 +0500

    changes

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-let usernames = [];
+function validate() {
+    var username = document.getElementById("username").value;
+    var message = document.getElementById("message");
 
-function checkAvailability() {
-  const username = document.getElementById('usernameInput').value;
-  const isAvailable = isUsernameAvailable(username);
-
-  const messageElement = document.getElementById('availabilityMessage');
-  messageElement.textContent = isAvailable
-    ? 'This username is available'
-    : 'This username has already been taken';
+    var names = ["Nurbek", "Sunnat", "Javohir", "Rustam"];
+    if (names.includes(username)) {
+        message.innerHTML = "Username has already been taken.";
+    }
+    else{
+        message.innerHTML = "Username is available.";
+    }
 }
 
-function isUsernameAvailable(username) {
-  for (let name of usernames) {
-    if (name === username) {
-      return false;
-        }
-  }
-  usernames.push(username);
-  return true; 
-}
+// let usernames = [];
+
+// function checkAvailability() {
+//   const username = document.getElementById('usernameInput').value;
+//   const isAvailable = isUsernameAvailable(username);
+
+//   const messageElement = document.getElementById('availabilityMessage');
+//   messageElement.textContent = isAvailable
+//     ? 'This username is available'
+//     : 'This username has already been taken';
+// }
+
+// function isUsernameAvailable(username) {
+//   for (let name of usernames) {
+//     if (name === username) {
+//       return false;
+//         }
+//   }
+//   usernames.push(username);
+//   return true; 
+// }
