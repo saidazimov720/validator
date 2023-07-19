a8afb4ec6c700e0db2e859696123448a23a974fe
saidazimov720 <saidazimovnurbek01@gmail.com>
2023-07-19 15:19:32 +0500

    add the array

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ function validate() {
     var username = document.getElementById("username").value;
     var messagel = document.getElementById("message");
 
+    var names = ["Nurbek", "Sunnat", "Javohir", "Rustam"];
     
 }
